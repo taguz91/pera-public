d65b009601bacba1a2d750ee6d60b009a1e2dac4
Gustavo <johnnygar98@hotmail.com>
2019-08-03 17:03:18 -0500

    Iniciando el proyecto

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+//Path de nuestra WEB
+define('URL', 'http://localhost/pera-public/');
+
+//Constantes de conexion a la base de datos
+define('HOST', 'localhost');
+define('DB', 'BDInstitutoJunio2019');
+define('PORT', '5433');
+define('USER', 'postgres');
+define('PASS', 'Holapostgres');
+define('CHARSET', 'utf8mb4');
+
+?>
