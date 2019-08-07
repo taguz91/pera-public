18f27033f3cdb980e35164745bbbc1401aac834b
Gustavo <johnnygar98@hotmail.com>
2019-08-07 15:25:37 -0500

    Solo un insert para realizar pruebas de login

---
+++ BD/Inserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO public."UsersWeb" (
+  id_persona, user_tipo, user_name,
+  user_clave)
+  VALUES (
+    1, 'alumno',
+    'taguz', md5('123')
+  )
--- BD/NuevasTablasBD.sql
+++ BD/NuevasTablasBD.sql
@@ -3,7 +3,7 @@ CREATE TABLE "UsersWeb" (
   id_persona integer NOT NULL,
   user_tipo character varying(50) NOT NULL,
   user_name character varying(50) NOT NULL UNIQUE,
-  user_clave bytea NOT NULL,
+  user_clave character varying(255) NOT NULL,
   user_activo boolean NOT NULL DEFAULT 'true',
   CONSTRAINT user_web_pk PRIMARY KEY ("id_user_web")
 ) WITH (OIDS = FALSE);
