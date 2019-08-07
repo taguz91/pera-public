66b2eadec6eafad43e8a22dd0d4338715b8c13d4
Johnny <johnnygar98@hotmail.com>
2019-08-07 15:08:46 -0500

    Creando las vistas

--- src/vista/static/perfil.php
+++ src/vista/static/perfil.php
@@ -4,13 +4,26 @@ require 'src/vista/templates/header.php';
 
 <div class="bg-blue">
   <div class="container">
-    <div class="row">
+    <div class="row py-5">
 
-      <div class="col-md-4 my-5 mx-center">
-        <img src="http://brownmead.academy/wp-content/uploads/2017/01/avatar.jpg" alt="" width="200px" height="200px" class="rounded">
+      <div class="col-md-4 mx-center">
+        <img src="http://brownmead.academy/wp-content/uploads/2017/01/avatar.jpg" alt="" width="200px" height="200px" class="d-block rounded mx-auto">
       </div>
 
       <div class="col-md-8">
+
+        <div class="text-white">
+          <h1>
+            <span class="badge badge-info">Nombres: </span>
+
+            <span class="badge">
+              <?php
+              global $user;
+              echo $user->primerNombre .' '.$user->primerApellido; ?>
+            </span>
+
+            </h1>
+        </div>
 
       </div>
 
