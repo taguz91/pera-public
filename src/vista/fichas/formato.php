56bdd161ffc69ccb2bba0418bc6515dadf4a9a7d
Johnny <johnnygar98@hotmail.com>
2019-08-29 22:05:59 -0500

    Mejorando el JS

--- src/vista/fichas/formato.php
+++ src/vista/fichas/formato.php
@@ -1,3 +1,5 @@
+
+
 <?php foreach ($secciones as $s) :?>
   <div class="row my-3 seccion">
 
@@ -7,7 +9,7 @@
           <?php echo $s['seccion_ficha_nombre']; ?>
         </h2>
       </div>
-      
+
       <?php if (isset($s['preguntas'])): ?>
 
           <?php foreach ($s['preguntas'] as $vp => $p): ?>
@@ -24,18 +26,34 @@
 
                 <?php foreach ($p['respuestas'] as $vr => $r): ?>
 
-                  <div class="custom-control custom-radio">
-                   <input
-                   onchange="<?php echo isset($act) ? "actualizar('" . $p['actualizar'] . "', '". $r['id_respuesta_ficha']."')" : '' ?>"
-                   value="<?php echo $r['id_respuesta_ficha']; ?>" type="radio" class="custom-control-input"
-                   id="<?php echo $vp.$vr.$r['id_respuesta_ficha']; ?>" name="<?php echo $vp.'n'.$p['id_pregunta_ficha']; ?>"
-                   <?php if($r['id_respuesta_ficha'] == $p['respuesta']){ echo "checked";}?>>
-                   <label class="custom-control-label" for="<?php echo $vp.$vr.$r['id_respuesta_ficha']; ?>"><?php echo $r['respuesta_ficha']; ?></label>
-                 </div>
+                  <?php
+                  if($p['pregunta_ficha_respuesta_tipo'] == 1){
+                    formRespuestaUnica(
+                      isset($act) ? $act : false,
+                      $vp,
+                      $vr,
+                      $p['actualizar'],
+                      $p['respuesta'],
+                      $p['id_pregunta_ficha'],
+                      $r['id_respuesta_ficha'], $r['respuesta_ficha']
+                    );
+                  }
+                  ?>
+
 
                 <?php endforeach; ?>
 
               <?php endif; ?>
+
+              <?php
+              if ($p['pregunta_ficha_respuesta_tipo'] == 3) {
+                formRespuestaLibreUnica();
+              }
+
+              if($p['pregunta_ficha_respuesta_tipo'] == 4){
+                formRespuestaLibreMultiple();
+              }
+              ?>
 
             </div>
 
@@ -52,9 +70,59 @@
 <?php endforeach; ?>
 
 
-<?php if (isset($act)): ?>
+<?php
+function formRespuestaUnica($act, $vp, $vr, $idActualizar, $idRespuesta,
+$idPreguntaFicha, $idRespuestaFicha, $respuesta){ ?>
+
+  <div class="custom-control custom-radio">
+   <input onchange=<?php
+   if($act){
+     echo '"'."actualizar('" . $idActualizar . "', '". $idRespuestaFicha."')".'"';
+   }else{
+     echo '""';
+   }
+   ?> value="<?php echo $idRespuestaFicha; ?>"
+   type="radio"
+   class="custom-control-input"
+   id="<?php echo $vp.$vr.$idRespuestaFicha; ?>"
+   name="<?php echo $vp.'n'.$idPreguntaFicha; ?>"
+   <?php if($idRespuestaFicha == $idRespuesta){ echo "checked";}?>>
+  <label class="custom-control-label"
+   for="<?php echo $vp.$vr.$idRespuestaFicha; ?>">
+   <?php echo $respuesta; ?>
+  </label>
+ </div>
+
+<?php } ?>
+
+
+<?php function formRespuestaLibreUnica(){ ?>
+  <div class="form-group">
+    <input class="form-control" type="text" name="" value="">
+  </div>
+<?php } ?>
+
+<?php function formRespuestaLibreMultiple(){ ?>
+  <div class="form-horizontal form-res-mul">
+    <div class="form-row">
+      <div class="col-10">
+        <div class="form-group">
+          <input class="form-control res-mul" type="text" name="" value="">
+        </div>
+      </div>
+      <div class="col-2">
+        <button class="btn btn-success btn-block btn-mas-txt" type="button" name="button">Mas</button>
+      </div>
+    </div>
+
+  </div>
+<?php } ?>
+
+<?php if (isset($act)):;?>
   <script type="text/javascript">
     const URLACT = '<?php echo constant('URL').'api/v1/ficha/guardar/?socioeconomica=asas'?>';
+
+    console.log('Cargamos excelentemente: '+URLACT);
 
     function act(id, id2){
       console.log('ID: '+id
@@ -81,3 +149,71 @@
     }
   </script>
 <?php endif; ?>
+
+
+<script type="text/javascript">
+  const BTNSMASTXT = document.querySelectorAll('.btn-mas-txt');
+  var vclick = 0;
+  BTNSMASTXT.forEach(b => {
+    /*b.addEventListener('click', function(){
+      console.log('Agregaaa');
+      agregarOtroTxtResMul();
+    });*/
+    b.onclick = agregarOtroTxtResMul;
+  });
+
+  agregarVal('res-mul');
+
+
+  function agregarOtroTxtResMul(){
+    vclick++;
+    let FROMRESMUL = document.querySelectorAll('.form-res-mul');
+
+    FROMRESMUL.forEach(f => {
+      let D1 = document.createElement('div');
+      let D2 = document.createElement('div');
+      let D3 = document.createElement('div');
+      let I = document.createElement('input');
+
+      D1.classList.add('form-row', 'c'+vclick);
+      D2.classList.add('col-10');
+      D3.classList.add('form-group');
+      I.classList.add('form-control', 'res-mul'+vclick);
+
+      D3.appendChild(I);
+      D2.appendChild(D3);
+      D1.appendChild(D2);
+
+      f.appendChild(D1);
+
+    });
+    agregarVal('res-mul'+vclick)
+  }
+
+  function agregarVal(clase){
+    let I = document.querySelectorAll('.'+clase);
+    I.forEach(i =>{
+      i.onblur = function(){
+        console.log('Periooo el focuss ' + this.value);
+        valirdarTodosLlenos(clase);
+      }
+    });
+  }
+
+  function valirdarTodosLlenos(clase){
+    let I = document.querySelectorAll('.'+clase);
+    let llenos = true;
+    I.forEach(i => {
+      if(i.value == ''){
+        llenos = false;
+      }
+    });
+
+    if(llenos){
+      console.log('Todos llenos podemos guardar!!!');
+    }else{
+      console.log('No estan todos llenos aun no guardaremos!');
+    }
+  }
+
+</script>
